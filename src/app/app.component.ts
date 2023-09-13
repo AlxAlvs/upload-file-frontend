b5485b46961ce1ec5cdb979ed5246bee80097d93
Alex Alves <alxalvs0121@gmail.com>
2023-09-13 13:42:32 -0300

    add initial configuration and xml parser

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { XmlParser } from './xmlParser';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'upload-files';
+  srcResult: any = {};
+  file: any = null;
+  
+  readFile = (e:any) => {
+    this.file = e.target.files[0];
+    
+    if (!this.file) {
+        return;
+    }
+    const reader = new FileReader();
+    reader.onload = (evt) => {
+        const xmlData: string = (evt as any).target.result;
+        
+        this.srcResult = XmlParser.xmlToObj(xmlData);
+        console.log(this.srcResult)
+    };
+    reader.readAsText(this.file);
+  }
+
 }
+
